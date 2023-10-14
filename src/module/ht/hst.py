0af3ff61b7cab6ff08965bbba219f72b659b7fd3
diaoenmao <diao_em@hotmail.com>
2023-10-14 15:36:53 -0400

    add theoretical estimates

--- src/module/ht/hst.py
+++ src/module/ht/hst.py
@@ -1,8 +1,14 @@
 import copy
 import numpy as np
 import torch
+import torch.nn as nn
 import model
 from config import cfg
+
+
+def stable_log_mean_exp(v):
+    m = v.max()
+    return m + (v - m).exp().mean().log()
 
 
 class HST:
@@ -10,7 +16,54 @@ class HST:
         super().__init__()
 
     def compute_fpr_tpr_theoretical(self, null, alter, null_model, alter_model, threshold):
-        pass
+        def fpr_objective(theta, T):
+            exponent = theta * (self.score(null, null_model, alter_model) / 2)
+            obj = stable_log_mean_exp(exponent) - theta * T
+            return obj
+
+        def fnr_objective(theta, T):
+            exponent = theta * (-self.score(alter, null_model, alter_model) / 2)
+            obj = stable_log_mean_exp(exponent) + theta * T
+            return obj
+
+        def fpr_closure():
+            fpr_optimizer.zero_grad()
+            loss = fpr_objective(log_fpr_theta_i.exp(), threshold_i)
+            loss.backward()
+            return loss
+
+        def fnr_closure():
+            fnr_optimizer.zero_grad()
+            loss = fnr_objective(log_fnr_theta_i.exp(), threshold_i)
+            loss.backward()
+            return loss
+
+        threshold[threshold == -float('inf')] = 10 * threshold[torch.isfinite(threshold)].min()
+        threshold[threshold == float('inf')] = 10 * threshold[torch.isfinite(threshold)].max()
+        fpr, fnr = [], []
+        fpr_log_theta = nn.ParameterList(
+            [nn.Parameter(torch.zeros(1, device=null.device)) for _ in range(len(threshold))])
+        fnr_log_theta = nn.ParameterList(
+            [nn.Parameter(torch.zeros(1, device=null.device)) for _ in range(len(threshold))])
+        for i in range(len(threshold)):
+            threshold_i = threshold[i]
+            log_fpr_theta_i = fpr_log_theta[i]
+            log_fnr_theta_i = fnr_log_theta[i]
+            fpr_optimizer = torch.optim.Adam([log_fpr_theta_i], lr=1)
+            fnr_optimizer = torch.optim.Adam([log_fnr_theta_i], lr=1)
+            for _ in range(10):
+                fpr_optimizer.step(fpr_closure)
+                fnr_optimizer.step(fnr_closure)
+            fpr_i = (fpr_objective(log_fpr_theta_i.exp(), threshold_i)).exp().item()
+            fnr_i = (fnr_objective(log_fnr_theta_i.exp(), threshold_i)).exp().item()
+            print(log_fpr_theta_i.exp(), log_fnr_theta_i.exp(), fpr_i, fnr_i)
+            fpr.append(fpr_i)
+            fnr.append(fnr_i)
+        print(fpr)
+        print(fnr)
+        exit()
+
+        return
 
     def score(self, data, null_model, alter_model):
         """Calculate Hyvarinen Score Difference"""
--- src/module/ht/ht.py
+++ src/module/ht/ht.py
@@ -45,7 +45,8 @@ class HypothesisTest:
                     mask = torch.isfinite(score_i)
                     score_i = score_i[mask]
                     target_i = target[mask]
-                    fpr_i, _, threshold_i = roc_curve(target_i.cpu().numpy(), score_i.cpu().numpy(), drop_intermediate=False)
+                    fpr_i, _, threshold_i = roc_curve(target_i.cpu().numpy(), score_i.cpu().numpy(),
+                                                      drop_intermediate=False)
                     threshold_i = np.interp(idx, fpr_i, threshold_i)
                     threshold_i = torch.tensor(threshold_i, device=data.device)
                     threshold.append(threshold_i)
@@ -54,20 +55,20 @@ class HypothesisTest:
 
     def test(self, input):
         null, alter, null_param, alter_param = input['null'], input['alter'], input['null_param'], input['alter_param']
+        null_model = eval('model.{}(null_param).to(cfg["device"])'.format(cfg['model_name']))
+        if self.num_samples_emp is not None:
+            alter_model = []
+            alter_split = torch.split(alter, self.num_samples_emp, dim=0)
+            num_tests = 100
+            for i in range(num_tests):
+                alter_model_i = eval('model.{}(copy.deepcopy(null_model.params)).to(cfg["device"])'.format(
+                    cfg['model_name']))
+                alter_model_i.fit(alter_split[i])
+                alter_model.append(alter_model_i)
+        else:
+            alter_model = eval('model.{}(alter_param).to(cfg["device"])'.format(cfg['model_name']))
+        threshold = self.make_threshold(null, alter, null_model, alter_model)
         if self.ht_mode[0] in ['lrt', 'hst']:
-            null_model = eval('model.{}(null_param).to(cfg["device"])'.format(cfg['model_name']))
-            if self.num_samples_emp is not None:
-                alter_model = []
-                alter_split = torch.split(alter, self.num_samples_emp, dim=0)
-                num_tests = 100
-                for i in range(num_tests):
-                    alter_model_i = eval('model.{}(copy.deepcopy(null_model.params)).to(cfg["device"])'.format(
-                        cfg['model_name']))
-                    alter_model_i.fit(alter_split[i])
-                    alter_model.append(alter_model_i)
-            else:
-                alter_model = eval('model.{}(alter_param).to(cfg["device"])'.format(cfg['model_name']))
-            threshold = self.make_threshold(null, alter, null_model, alter_model)
             if self.ht_mode[1] == 'e':
                 with torch.no_grad():
                     data = torch.cat([null, alter], dim=0)
@@ -85,7 +86,17 @@ class HypothesisTest:
                         fpr = np.stack(fpr, axis=0).mean(axis=0)
                         fnr = np.stack(fnr, axis=0).mean(axis=0)
             elif self.ht_mode[1] == 't':
-                fpr, fnr = self.ht.compute_fpr_tpr_theoretical(null, alter, null_model, alter_model, threshold)
+                if not isinstance(alter_model, list):
+                    fpr, fnr = self.ht.compute_fpr_tpr_theoretical(null, alter, null_model, alter_model, threshold)
+                else:
+                    fpr, fnr = [], []
+                    for i in range(len(alter_model)):
+                        fpr_i, fnr_i = self.ht.compute_fpr_tpr_empirical(null, alter, null_model, alter_model[i],
+                                                                         threshold)
+                        fpr.append(fpr_i)
+                        fnr.append(fnr_i)
+                    fpr = np.stack(fpr, axis=0).mean(axis=0)
+                    fnr = np.stack(fnr, axis=0).mean(axis=0)
             else:
                 raise ValueError('Not valid ht mode')
         else:
